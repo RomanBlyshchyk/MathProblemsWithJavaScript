b711e73f671fff0f6dc12d9eea9a4a28c6b5ae75
Roman Blyshchyk <roman-bl@hotmail.com>
2018-10-25 18:30:55 -0400

    Continued work on the Factorial problem

---
+++ Problem1/factorial.js
@@ -0,0 +1,22 @@
+
+function calculateFactorial() {
+
+    let factorialInput = document.getElementById('factorialInput').value;
+    if(isNaN(factorialInput)){
+        console.log("input is not a NUMBER. Factorial function can only be used on a numeric type!")
+        // TODO: Handle it via UI somehow (eihter another "error field" or a MessageBox)
+    }else if(factorialInput === 0){
+        console.log(1)
+    }
+    else{
+        let factorial = 1;
+        while (factorialInput !== 0) {
+            factorial *= factorialInput
+            factorialInput--
+        }
+        console.log(factorialInput);
+    }
+        
+    
+    
+}
